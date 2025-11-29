53b4b9c2f0e942d90f471212bdbf9a223c675bbf
aDiamant03 <dva01082009@mail.ru>
2025-11-29 13:19:22 +0300

    xz

--- core/src/main/kotlin/menu/handlers/handlersMenu.kt
+++ core/src/main/kotlin/menu/handlers/handlersMenu.kt
@@ -5,3 +5,7 @@ package ChillZoneBot.core.src.main.kotlin.ReplyButtons.menu.handlers
 fun detectFalse(): String {
     return "выбрано false в BoolButton"
 }
+
+fun openMemes(): String {
+    return "Открываю мемы..."
+}
