6a28da54c517fb9ce753f6f8d7e67e1f05da0c69
V9kerNiene <garry_gan@outlook.com>
2025-11-30 15:40:47 +0300

    [upd] added inline to KeyboardMenu.kt

--- core/src/main/kotlin/keyboards/base/KeyboardMenu.kt
+++ core/src/main/kotlin/keyboards/base/KeyboardMenu.kt
@@ -1,5 +1,8 @@
 package ChillZoneBot.core.src.main.kotlin.keyboards.base.KeyboardMenu
 
+import ChillZoneBot.core.src.main.kotlin.InlineClass.BoolInlineButton
+import ChillZoneBot.core.src.main.kotlin.InlineClass.InlineButton
+import ChillZoneBot.core.src.main.kotlin.InlineClass.ChooseInlineButton
 import ChillZoneBot.core.src.main.kotlin.ReplyClass.BoolButton
 import ChillZoneBot.core.src.main.kotlin.ReplyClass.Button
 import ChillZoneBot.core.src.main.kotlin.ReplyClass.ChooseButton
@@ -29,6 +32,40 @@ fun getKeyboardMenu(): MutableList<MutableList<Button>> {
 
     val memeButton = Button(
         name = "Мемы"
+    )
+
+    return mutableListOf(
+        mutableListOf(button1, button2),
+        mutableListOf(button3),
+        mutableListOf(memeButton)
+    )
+}
+
+fun getInlineKeyboardMenu(): MutableList<MutableList<InlineButton>> {
+    val button1 = InlineButton(
+        name = "Button",
+        callback = "callback1"
+    )
+    val button2 = ChooseInlineButton(
+        name = "ChooseButton",
+        list = mapOf(
+            "name1" to null,
+            "name2" to null,
+            "name3" to null
+        ),
+        startIndex = 0,
+        callback = "callback2"
+    )
+    val button3 = BoolInlineButton(
+        name = "BoolButton",
+        funcFalse = ::detectFalse,
+        flag = false,
+        callback = "callback3"
+    )
+
+    val memeButton = InlineButton(
+        name = "Мемы",
+        callback = "callback4"
     )
 
     return mutableListOf(
